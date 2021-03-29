63965d9269a18ddbd005687ef2801112e8994866
Seungjoo Kim <sjskybone@gmail.com>
2021-03-29 16:01:55 -0700

    Added input eventlistener to update the value of src attribute. #1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 /* global data */
 /* exported data */
+var $placeholderImage = document.querySelector('#placeholder-image');
+var $photoUrl = document.querySelector('.photo-url');
+
+$photoUrl.addEventListener('input', imgInputHandler);
+
+function imgInputHandler(event) {
+  $placeholderImage.setAttribute('src', event.target.value);
+}
