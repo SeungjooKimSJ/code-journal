3a6c1fed683e982a83cd5a9690686538505bfe6a
Seungjoo Kim <sjskybone@gmail.com>
2021-03-29 17:07:31 -0700

    Added submit eventlistener on the form element. #1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 /* global data */
 /* exported data */
 var $placeholderImage = document.querySelector('#placeholder-image');
-var $photoUrl = document.querySelector('.photo-url');
+var $photoUrl = document.querySelector('.url');
 
 $photoUrl.addEventListener('input', imgInputHandler);
 
 function imgInputHandler(event) {
   $placeholderImage.setAttribute('src', event.target.value);
+}
+
+var $formNewEntry = document.querySelector('.form-new-entry');
+
+$formNewEntry.addEventListener('submit', submitNewEntryForm);
+
+function submitNewEntryForm(event) {
+  event.preventDefault();
+
+  var newEntryData = {
+    title: $formNewEntry.elements.title.value,
+    url: $formNewEntry.elements.url.value,
+    notes: $formNewEntry.elements.notes.value
+  };
+
+  var dataNextId = data.nextEntryId;
+  newEntryData.nextEntryId = dataNextId;
+
+  data.nextEntryId++;
+
+  data.entries.unshift(newEntryData);
+
+  $placeholderImage.setAttribute('src', './images/placeholder-image-square.jpg');
+
+  $formNewEntry.reset();
 }
